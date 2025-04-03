7108fabc02952c0f93599d1c2dca80076981c806
Luz Dary Crespo <luzdaricrespo@gmail.com>
2025-04-03 13:11:46 -0500

    style: add view expediente Preso

---
+++ src/View/RoundedPanel.java
@@ -0,0 +1,24 @@
+
+package View;
+
+
+import java.awt.*;
+import javax.swing.*;
+
+public class RoundedPanel extends JPanel {
+    private int cornerRadius;
+
+    public RoundedPanel(int radius) {
+        this.cornerRadius = radius;
+        setOpaque(false); 
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground()); 
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);
+    }
+}
